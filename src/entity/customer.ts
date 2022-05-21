fdf272c37798ace22d4d86aa5a872c2d295cedaa
vitor-melo <vitorpmelo20@gmail.com>
2022-05-20 21:06:44 -0300

    anemic class

---
+++ src/entity/customer.ts
@@ -0,0 +1,36 @@
+class Customer {
+
+    _id: string;
+    _name: string;
+    _address: string;
+
+    constructor(id: string, name:string, address:string) {
+        this._id = id;
+        this._name = name;
+        this._address = address;
+    }
+
+    get id(): string {
+        return this._id;
+    }
+
+    set id(id: string) {
+        this._id = id;
+    }
+
+    get name(): string {
+        return this._id;
+    }
+
+    set name(name: string) {
+        this._name = name;
+    }
+
+    get address(): string {
+        return this._address;
+    }
+
+    set address(address: string) {
+        this._address = address;
+    }
+}
