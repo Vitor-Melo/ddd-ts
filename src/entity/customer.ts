f617ed1e18c90164ddf03a8f1ae39ca784ca9501
vitor-melo <vitorpmelo20@gmail.com>
2022-05-20 21:32:57 -0300

    validations and business rules

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -3,24 +3,38 @@ class Customer {
     _id: string;
     _name: string;
     _address: string;
-    _active: boolean = true;
+    _active: boolean = false;
 
     constructor(id: string, name:string, address:string) {
         this._id = id;
         this._name = name;
         this._address = address;
+        this.validate();
+    }
+
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error('Name is required');
+        }
+
+        if (this._id.length === 0) {
+            throw new Error('ID is required')
+        }
     }
 
     changeName(name: string) {
         this._name = name
+        this.validate()
     }
 
     activate() {
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer")
+        }
         this._active = true
     }
 
     desactivate() {
         this._active = false
     }
-
 }
