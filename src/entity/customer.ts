af637c0d66609dbbf74d081c61ab29a05a25a6fc
vitor-melo <vitorpmelo20@gmail.com>
2022-05-21 10:22:13 -0300

    customer tests

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,10 +2,10 @@ import Address from "./address";
 
 export default class Customer {
 
-    _id: string;
-    _name: string;
-    _address!: Address;
-    _active: boolean = false;
+    private _id: string;
+    private _name: string;
+    private _address!: Address;
+    private _active: boolean = false;
 
     constructor(id: string, name:string) {
         this._id = id;
@@ -23,6 +23,10 @@ export default class Customer {
         }
     }
 
+    get name(): string {
+        return this._name;
+    }
+
     changeName(name: string) {
         this._name = name
         this.validate()
@@ -33,6 +37,10 @@ export default class Customer {
             throw new Error("Address is mandatory to activate a customer")
         }
         this._active = true
+    }
+
+    isActivate(): boolean {
+        return this._active;
     }
 
     set Address(address: Address) {
