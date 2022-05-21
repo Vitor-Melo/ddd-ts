c9e151cbea7b33034cac5efb7275116bee7780dd
vitor-melo <vitorpmelo20@gmail.com>
2022-05-21 16:17:40 -0300

    reward points in customer

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -65,4 +65,18 @@ describe("Customer unit tests", () => {
         }).toThrowError('Address is mandatory to activate a customer');
 
     });
+
+    it("should add reward points", () => {
+        const customer = new Customer("1", "Customer 1");
+
+        expect(customer.rewardPoints).toBe(0);
+
+        customer.addRewardPoints(10);
+
+        expect(customer.rewardPoints).toBe(10);
+
+        customer.addRewardPoints(10);
+
+        expect(customer.rewardPoints).toBe(20);
+    });
 });
