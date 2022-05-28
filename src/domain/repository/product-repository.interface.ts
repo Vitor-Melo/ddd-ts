711ecb94a3b82da3d30efd29cb83108d2d8e72d8
vitor-melo <vitorpmelo20@gmail.com>
2022-05-28 10:29:08 -0300

    change directory, repository domain, infrastructure

---
+++ src/domain/repository/product-repository.interface.ts
@@ -0,0 +1,8 @@
+import Product from "../entity/product";
+import RepositoryInterface from "./repository-interface";
+
+export default interface ProductRepositoryInterface 
+    extends RepositoryInterface<Product> {
+    
+        
+}
