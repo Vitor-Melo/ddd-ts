c3205548440f0ce7155d727152861da52a3a6888
vitor-melo <vitorpmelo20@gmail.com>
2022-05-28 12:13:03 -0300

    implement product repository

--- src/domain/entity/product.ts
+++ src/domain/entity/product.ts
@@ -38,6 +38,10 @@ export default class Product {
         this.validate();
     }
 
+    get id(): string {
+        return this._id;
+    }
+
     get name(): string {
         return this._name;
     }
---
+++ src/infrastructure/repository/product.repository.ts
@@ -0,0 +1,52 @@
+import Product from "../../domain/entity/product";
+import product from "../../domain/entity/product";
+import ProductRepositoryInterface from "../../domain/repository/product-repository.interface";
+import ProductModel from "../db/sequelize/model/product.model";
+
+export default class ProductRepository implements ProductRepositoryInterface {
+    
+    async create(entity: product): Promise<void> {
+
+        await ProductModel.create({
+            id: entity.id,
+            name: entity.name,
+            price: entity.price
+        });
+    }
+
+    async update(entity: product): Promise<void> {
+
+        await ProductModel.update(
+            {
+                name: entity.name,
+                price: entity.price
+            }, 
+            {
+                where: {
+                    id: entity.id
+                }
+            }
+        );
+
+    }
+    async find(id: string): Promise<Product> {
+        
+        const productModel = await ProductModel.findOne({
+            where: { id }
+        })
+
+        return new Product(
+            productModel.id,
+            productModel.name,
+            productModel.price
+        );
+
+    }
+
+    async findAll(): Promise<Product[]> {
+        const productModels = await ProductModel.findAll();
+        return productModels.map((productModels) => 
+            new Product(productModels.id, productModels.name, productModels.price)
+        );
+    }
+}
