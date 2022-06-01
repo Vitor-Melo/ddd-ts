c77942001e5e175a23bc480dbb96a0005a4dd84b
vitor-melo <vitorpmelo20@gmail.com>
2022-06-01 08:27:56 -0300

    create order repository interface, implements methods interface

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,8 +1,21 @@
 import Order from "../../domain/entity/order";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.inteface";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
+    update(entity: Order): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    
+    find(id: string): Promise<Order> {
+        throw new Error("Method not implemented.");
+    }
+
+    findAll(): Promise<Order[]> {
+        throw new Error("Method not implemented.");
+    }
+
     async create(entity: Order): Promise<void> {
         await OrderModel.create({
             id: entity.id,
