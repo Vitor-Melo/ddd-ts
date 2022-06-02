3ef34d2916580022e31147944f321cdfc613578e
vitor-melo <vitorpmelo20@gmail.com>
2022-06-02 11:29:06 -0300

    test dispatch event customerCreatedEvent

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,3 +1,6 @@
+import CustomerCreatedEvent from "../customer/customer-created-event";
+import SendConsoleLog1Handler from "../customer/handler/send-console-log1.handler";
+import SendConsoleLog2Handler from "../customer/handler/send-console-log2.handler";
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
 import ProductCreatedEvent from "../product/product-created-event";
 import EventDispatcher from "./event-dispatcher";
@@ -65,5 +68,30 @@ describe("Domain events tests", () => {
         eventDispatcher.notify(productCreatedEvent);
         expect(spyEventHandler).toHaveBeenCalled();
     });
+
+    it("should notity send console log 1 and console log 2 event handler", () =>  {
+        
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler1 = new SendConsoleLog1Handler();
+        const eventHandler2 = new SendConsoleLog2Handler();
+        const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+        const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler1);
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]).toMatchObject(eventHandler2);
+
+        const customerCreatedEvent = new CustomerCreatedEvent({
+            id: "123",
+            name: "Vitor",
+        });
+
+        eventDispatcher.notify(customerCreatedEvent);
+
+        expect(spyEventHandler1).toHaveBeenCalled();
+        expect(spyEventHandler2).toHaveBeenCalled();
+    })
 
 });
