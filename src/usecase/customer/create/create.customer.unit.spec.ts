d16a18f2c601f1c2a0d73405a272461c70fd3b1f
vitor-melo <vitorpmelo20@gmail.com>
2022-06-04 19:17:58 -0300

    create customer use case, and unit test

---
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -0,0 +1,42 @@
+import CreateCustomerUseCase from "./create.customer.usecase";
+
+const input = {
+    name: "John",
+    address: {
+        street: "Street",
+        number: 123,
+        zip: "zip",
+        city: "City"
+    }
+};
+
+const mockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn()
+    }
+} 
+
+describe("Unit test create customer use case", () => {
+
+    it("should create a customer", async () => {
+        const customerRepository = mockRepository();
+        const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+        const output = await customerCreateUseCase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            address: {
+                street: input.address.street,
+                number: input.address.number,
+                zip: input.address.zip,
+                city: input.address.city
+            }
+        });
+    });
+
+})
